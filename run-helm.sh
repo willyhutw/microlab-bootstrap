8b6d6b2d63b8183b02f4039bf84912006261f2de
Willy Hu <willyhu.tw@gmail.com>
2025-05-19 14:33:46 +0800

    Update core helm chart resource/valuse and the run-helm script.

--- run-helm.sh
+++ run-helm.sh
@@ -11,19 +11,22 @@ main() {
   helm repo update
 
   # cilium
-  helm upgrade --install -n kube-system cilium cilium/cilium --create-namespace -f ./helm-values/cilium.yml --version 1.17.2 --wait
+  helm upgrade --install -n kube-system cilium cilium/cilium --create-namespace -f ./helm-values/cilium.yml --version 1.17.4 --wait
   kubectl apply -f resources/cilium-ippool.yml
 
   # istio
-  helm upgrade --install -n istio-system istio-base istio/base --create-namespace --version 1.24.3 --wait
-  helm upgrade --install -n istio-system istiod istio/istiod --create-namespace -f ./helm-values/istiod.yml --version 1.24.3 --wait
-  helm upgrade --install -n istio-system istio-ingressgateway istio/gateway --create-namespace -f ./helm-values/istio-ingressgateway.yml --version 1.24.3 --wait
+  helm upgrade --install -n istio-system istio-base istio/base --create-namespace --version 1.25.3 --wait
+  helm upgrade --install -n istio-system istiod istio/istiod --create-namespace -f ./helm-values/istiod.yml --version 1.25.3 --wait
+  # istio - ingressgateway internal
+  helm upgrade --install -n istio-system istio-igw-internal istio/gateway --create-namespace -f ./helm-values/istio-igw-internal.yml --version 1.25.3 --wait
+  # istio - ingressgateway external
+  helm upgrade --install -n istio-system istio-igw-external istio/gateway --create-namespace -f ./helm-values/istio-igw-external.yml --version 1.25.3 --wait
 
   # metrics-server
   helm upgrade --install -n kube-system metrics-server metrics-server/metrics-server -f ./helm-values/metrics-server.yml --version 3.12.2 --wait
 
   # cert-manager
-  helm upgrade --install -n cert-manager cert-manager jetstack/cert-manager --create-namespace --set installCRDs=true --version 1.17.1 --wait
+  helm upgrade --install -n cert-manager cert-manager jetstack/cert-manager --create-namespace --set installCRDs=true --version 1.17.2 --wait
 
   # letsencrypt cluster-issuer
   export CLOUDFLARE_ACME_SECRET=$(cat ~/cloudflare_acme_token.txt)
@@ -33,11 +36,11 @@ main() {
   unset CLOUDFLARE_ACME_SECRET
 
   # cilium-hubble
-  helm upgrade --install -n kube-system cilium cilium/cilium --reuse-values -f ./helm-values/cilium-hubble.yml --version 1.17.2 --wait
+  helm upgrade --install -n kube-system cilium cilium/cilium --reuse-values -f ./helm-values/cilium-hubble.yml --version 1.17.4 --wait
   kubectl apply -f resources/hubble
 
   # argocd
-  helm upgrade --install -n argocd argocd argo/argo-cd --create-namespace -f ./helm-values/argocd.yml --version 7.8.13 --wait
+  helm upgrade --install -n argocd argocd argo/argo-cd --create-namespace -f ./helm-values/argocd.yml --version 7.8.28 --wait
   kubectl apply -f resources/argocd
 
   # apply storage-class, pv, pvc and configmap for internal monitoring services.
