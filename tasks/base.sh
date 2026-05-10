e459cb0de689331639dabcceef0c2a5908f8d70a
Willy Hu <willyhu.tw@gmail.com>
2026-05-10 18:19:37 +0800

    base: add open-webui and qdrant data dirs

--- tasks/base.sh
+++ tasks/base.sh
@@ -46,6 +46,8 @@ create_data_dirs() {
   sudo mkdir -p /mnt/data/grafana
   sudo mkdir -p /mnt/data/prometheus
   sudo mkdir -p /mnt/data/loki
+  sudo mkdir -p /mnt/data/open-webui
+  sudo mkdir -p /mnt/data/qdrant
 }
 
 main() {
